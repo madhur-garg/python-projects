0e6df13217b81c1abc010d5a12896a06b6a31cf0
madhur-garg <63413053+madhur-garg@users.noreply.github.com>
2020-04-23 13:46:25 +0530

    Update guess_the_number_game.py

--- guess_the_number_game.py
+++ guess_the_number_game.py
@@ -1,20 +1,45 @@
 import random
+a = int(input("Enter the value of a\n"))
+b = int(input("Enter the value of b\n"))
 
-n = random.randint(1, 99)
-i = 0
-print("number between 0 and 100")
-print("This is game in which you will guess the number which i already choose. You have only 9 guesses")
-while i <= 9:
-    if i == 9:
-        print("Game Over")
-    else:
-        g1 = int(input("guess the number\n"))
-        if g1 == n:
-            print("congratulation! you won the game")
-            break
-        elif g1 > n:
-            print("sorry but actual number is lesser then", g1)
+n = random.randint(a+1, b-1)
+
+
+def game_loop():
+    print("Player 1:")
+    i = 1
+    global n
+    while i:
+        num = int(input(f"Please guess the number between {a} and {b}\n"))
+        if num > n:
+            print("Wrong guess, try a smaller number again")
+        elif num < n:
+            print("Wrong guess, try a greater number again")
         else:
-            print("sorry but actual number is greater then", g1)
-        print("you have left", 8-i, "guesses")
-    i += 1
+            print(f"Correct you took {i} trials to guess the number\n")
+            print("Player 2:")
+
+            n = random.randint(a + 1, b - 1)
+            j = 1
+            while j:
+                num = int(input(f"Please guess the number between {a} and {b}\n"))
+                if num > n:
+                    print("Wrong guess, try a smaller number again")
+                elif num < n:
+                    print("Wrong guess, try a greater number again")
+                else:
+                    print(f"Correct you took {j} trials to guess the number\n")
+                    if i < j:
+                        print("Player 1 wins!!")
+                        quit()
+                    elif i > j:
+                        print("Player 2 wins!!")
+                        quit()
+                    else:
+                        print("Game tie. Play again\n")
+                        game_loop()
+                j += 1
+        i += 1
+
+
+game_loop()
