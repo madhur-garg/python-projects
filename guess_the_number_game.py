030b25114005c226b6e249896b91909d7d0a7eeb
madhur-garg <63413053+madhur-garg@users.noreply.github.com>
2020-04-23 14:00:53 +0530

    Update guess_the_number_game.py

--- guess_the_number_game.py
+++ guess_the_number_game.py
@@ -6,27 +6,27 @@ n = random.randint(a+1, b-1)
 
 
 def game_loop():
-    print("Player 1:")
+    print("Player 1's turn:")
     i = 1
     global n
     while i:
         num = int(input(f"Please guess the number between {a} and {b}\n"))
         if num > n:
-            print("Wrong guess, try a smaller number again")
+            print("Wrong guess, Enter a smaller number")
         elif num < n:
-            print("Wrong guess, try a greater number again")
+            print("Wrong guess, Enter a bigger number")
         else:
             print(f"Correct you took {i} trials to guess the number\n")
-            print("Player 2:")
+            print("Player 2's turn:")
 
             n = random.randint(a + 1, b - 1)       # Change the value of n for second player
             j = 1
             while j:
                 num = int(input(f"Please guess the number between {a} and {b}\n"))
                 if num > n:
-                    print("Wrong guess, try a smaller number again")
+                    print("Wrong guess, Enter a smaller number")
                 elif num < n:
-                    print("Wrong guess, try a greater number again")
+                    print("Wrong guess, Enter a smaller number")
                 else:
                     print(f"Correct you took {j} trials to guess the number\n")
                     if i < j:
